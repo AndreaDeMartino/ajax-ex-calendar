c23f1f45fa5ae9a3ce6effcca8cb20ff60d4a9d7
Andrea De Martino <ademartino1992@gmail.com>
2020-04-29 15:29:32 +0200

    Fix: Minor Fix

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
 	// Left Click
 	left.click(function(){
 		if (baseMonth.month() > 0){
-			baseMonth = baseMonth.subtract(1,'months');
+			baseMonth = baseMonth.subtract(1,'M');
 			$('.month-list').children().remove();
 			printMonth(template, baseMonth);
 			printHoliday(baseMonth);
@@ -34,7 +34,7 @@ $(document).ready(function () {
 	// Right Click
 	right.click(function(){
 		if (baseMonth.month() < 11){
-		baseMonth = baseMonth.add(1,'months');
+		baseMonth = baseMonth.add(1,'M');
 		$('.month-list').children().remove();
 		printMonth(template, baseMonth);
 		printHoliday(baseMonth);
@@ -50,63 +50,63 @@ $(document).ready(function () {
 
 // Stampa a schermo i giorni del mese
 function printMonth(template, date) {
-    // numero giorni nel mese
-    var daysInMonth = date.daysInMonth();
+	// numero giorni nel mese
+	var daysInMonth = date.daysInMonth();
 
-    //  setta header
-    $('h1').html( date.format('MMMM YYYY') );
+	//  setta header
+	$('h1').html( date.format('MMMM YYYY') );
 
-    // Imposta data attribute data visualizzata
-    $('.month').attr('data-this-date',  date.format('YYYY-MM-DD'));
+	// Imposta data attribute data visualizzata
+	$('.month').attr('data-this-date',  date.format('YYYY-MM-DD'));
 
-    // genera giorni mese
-    for (var i = 0; i < daysInMonth; i++) {
-        // genera data con moment js
-        var thisDate = moment({
-            year: date.year(),
-            month: date.month(),
-            day: i + 1
-        });
+	// genera giorni mese
+	for (var i = 0; i < daysInMonth; i++) {
+		// genera data con moment js
+		var thisDate = moment({
+			year: date.year(),
+			month: date.month(),
+			day: i + 1
+		});
 
-        // imposta dati template
-        var context = {
-            class: 'day',
-            day: thisDate.format('DD MMMM'),
-            completeDate: thisDate.format('YYYY-MM-DD')
-        };
+		// imposta dati template
+		var context = {
+			class: 'day',
+			day: thisDate.format('DD MMMM'),
+			completeDate: thisDate.format('YYYY-MM-DD')
+		};
 
-        //compilare e aggiungere template
-        var html = template(context);
-        $('.month-list').append(html);
-    }
+		//compilare e aggiungere template
+		var html = template(context);
+		$('.month-list').append(html);
+	}
 }
 
 // Ottieni e stampa festività
 function printHoliday(date) {
-    // chiamo API
-    $.ajax({
-        url: 'https://flynn.boolean.careers/exercises/api/holidays' ,
-        method: 'GET',
-        data: {
-            year: date.year(),
-            month: date.month()
-        },
-        success: function(res) {
-            var holidays = res.response;
+	// chiamo API
+	$.ajax({
+		url: 'https://flynn.boolean.careers/exercises/api/holidays' ,
+		method: 'GET',
+		data: {
+				year: date.year(),
+				month: date.month()
+		},
+		success: function(res) {
+			var holidays = res.response;
 
-            for (var i = 0; i < holidays.length; i++) {
-                var thisHoliday = holidays[i];
+			for (var i = 0; i < holidays.length; i++) {
+				var thisHoliday = holidays[i];
 
-                var listItem = $('li[data-complete-date="' + thisHoliday.date + '"]');
+				var listItem = $('li[data-complete-date="' + thisHoliday.date + '"]');
 
-                if(listItem) {
-                    listItem.addClass('holiday');
-                    listItem.text( listItem.text() + ' - ' + thisHoliday.name );
-                }
-            }
-        },
-        error: function() {
-            console.log('Errore chiamata festività');
-        }
-    });
+				if(listItem) {
+					listItem.addClass('holiday');
+					listItem.text( listItem.text() + ' - ' + thisHoliday.name );
+				}
+			}
+		},
+		error: function() {
+			console.log('Errore chiamata festività');
+		}
+	});
 }
